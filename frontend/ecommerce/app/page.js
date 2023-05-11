1ffff8aad2e95bc56535bfaf86ab4a18c2e2f327
Leon Erasmus <leondevcode@gmail.com>
2023-05-11 18:00:30 -0400

    added prettier.json

--- frontend/ecommerce/app/page.js
+++ frontend/ecommerce/app/page.js
@@ -1,18 +1,29 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import styles from './page.module.css';
 
 async function getMessage() {
-  const res = await fetch("http://localhost:1337/api/hellos/2", { next: { revalidate: 1 } }); // The result is cached
+  const res = await fetch('http://localhost:1337/api/hellos?locale=all', { next: { revalidate: 1 } }); // The result is cached
   const resdata = await res.json();
-  const message = resdata.data.attributes.message;
-  return message;
+  const data = resdata.data;
+  return data;
 }
 
 export default async function Home() {
   const message = await getMessage();
+  console.log(message);
   return (
-    <main className={styles.main}>
-      <h1>{message}</h1>
+    <main style={{ textAlign: 'center', marginTop: '250px' }}>
+      <p style={{ marginBottom: '35px' }}>fetch("http://localhost:1337/api/hellos?locale=all")</p>
+      {message &&
+        message.map((item) => {
+          return (
+            <>
+              <h2>
+                {item.attributes.message} : locale = {item.attributes.locale}
+              </h2>
+            </>
+          );
+        })}
     </main>
   );
 }
