1c78993b8e665149f1ce5a0229e0917d558a7d98
Leon Erasmus <leondevcode@gmail.com>
2023-05-11 17:15:40 -0400

    setup

--- frontend/ecommerce/app/page.js
+++ frontend/ecommerce/app/page.js
@@ -1,21 +1,18 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
 
 async function getMessage() {
-  const res = await fetch('http://localhost:1337/api/hellos/2',
-  { next : { revalidate : 1}}
-  ); // The result is cached
+  const res = await fetch("http://localhost:1337/api/hellos/2", { next: { revalidate: 1 } }); // The result is cached
   const resdata = await res.json();
-  const message = resdata.data.attributes.message
-  return message
+  const message = resdata.data.attributes.message;
+  return message;
 }
- 
 
 export default async function Home() {
-  const message = await getMessage()
+  const message = await getMessage();
   return (
     <main className={styles.main}>
-      <h1>{message}</h1> 
+      <h1>{message}</h1>
     </main>
-  )
+  );
 }
